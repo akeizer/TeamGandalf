beb97e07a16ad4a790b1fb6e3f8c1082c1bb58dd
Addison Keizer <wakeizer@gmail.com>
2016-10-01 17:41:29 -0400

    setup initial web, improve learning module encapsulation.

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
     "flag"
     "./imagetocsv"
     "./learning"
-    "github.com/sjwhitworth/golearn/evaluation"
+    "./web"
 )
 
 func helpText() {
@@ -15,16 +15,14 @@ func helpText() {
 
 func main() {
     totalpixels := 400
-
-    if (len(os.Args) < 2) {
+    var train = flag.Bool("train", false, "If we should use the inputs as training")
+    var help = flag.Bool("h", false, "Should we display the help information")
+    var isWeb = flag.Bool("web", false, "Setup webserver")
+    flag.Parse()
+    if len(os.Args) < 2 && !*isWeb {
         helpText()
         os.Exit(1)
     }
-
-    var train = flag.Bool("train", false, "If we should use the inputs as training")
-    var help = flag.Bool("h", false, "Should we display the help information")
-
-    flag.Parse()
 
     if *help {
         helpText()
@@ -32,29 +30,28 @@ func main() {
     }
     args := flag.Args()
     outfilename := args[0]
+    if *isWeb {
+      web.Serve()
+    } else {
+      // open output file
+      if !*train {
+          outfile, err := os.Create(outfilename)
+          if err != nil {
+              panic(fmt.Sprintf("Failed to create output file: %s ", err))
+          }
+          outfile.WriteString(imagetocsv.CreateHeaderRow(totalpixels) + "\n")
+          for _, arg := range args[1:] {
+              outfile.WriteString(imagetocsv.ConvertToCSV(arg) + "\n")
+          }
+          defer outfile.Close()
+      }
 
-    // open output file
-    if !*train {
-        outfile, err := os.Create(outfilename)
-        if err != nil {
-            panic(fmt.Sprintf("Failed to create output file: %s ", err))
-        }
-        outfile.WriteString(imagetocsv.CreateHeaderRow(totalpixels) + "\n")
-        for _, arg := range args[1:] {
-            outfile.WriteString(imagetocsv.ConvertToCSV(arg) + "\n")
-        }
-        defer outfile.Close()
+      // want this code here so that it doesnt tell us that learning is unused
+      // but also don't want it to fail while we change how the CSV is formatted
+      if (false) {
+          results := learning.PerformAnalysis();
+          fmt.Printf("Summary: %s", results.Summary)
+          fmt.Printf("Accuracy: %e", results.Accuracy)
+      }
     }
-
-    // want this code here so that it doesnt tell us that learning is unused
-    // but also don't want it to fail while we change how the CSV is formatted
-    if (false) {
-        train, test := learning.ReadTrainingTestData("trainingFile", "testFile")
-
-        c := learning.TrainAndClassifyData(train, test)
-
-        fmt.Println(evaluation.GetSummary(c))
-        fmt.Println(evaluation.GetAccuracy(c))
-    }
-
 }
